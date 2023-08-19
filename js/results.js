96f739ea8b73ccf264654ea5e68ec7e8d65499f5
yetdarkerdevelopment <yetdarkerdevelopment@gmail.com>
2023-08-19 21:30:21 +0200

    Debugging

--- js/results.js
+++ js/results.js
@@ -4,7 +4,11 @@ let background = new Image();
 background.src = "https://yetdarkerdevelopment.github.io/dndalignment/images/dndcompass.png";
 let law = urlParams.get("law") * 666 / 14 + 17;
 let evil = urlParams.get("evil") * 666 / 9 + 17;
+console.log(law);
+console.log(evil);
 let canvas = document.getElementById("result_dot_canvas");
+console.log(canvas.width);
+console.log(canvas.height);
 let context = canvas.getContext("2d");
 background.onload = function(){
     context.drawImage(background, 0, 0, canvas.width, canvas.height);   

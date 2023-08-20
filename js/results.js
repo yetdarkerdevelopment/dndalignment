863ab5a42a492f52c60e690cf68a3e691e8f1ba5
yetdarkerdevelopment <yetdarkerdevelopment@gmail.com>
2023-08-20 11:54:42 +0200

    Fix

--- js/results.js
+++ js/results.js
@@ -2,8 +2,10 @@ let queryString = window.location.search;
 let urlParams = new URLSearchParams(queryString);
 let background = new Image();
 background.src = "https://yetdarkerdevelopment.github.io/dndalignment/images/dndcompass.png";
-let law = urlParams.get("law") * 666 / 14 + 17;
-let evil = urlParams.get("evil") * 666 / 9 + 17;
+let dot = new Image();
+dot.src = "https://yetdarkerdevelopment.github.io/dndalignment/images/dnddot.png";
+let law = urlParams.get("law") * 333 / 14 + 333; // + 17;
+let evil = urlParams.get("evil") * 333 / 9 + 333; // + 17;
 console.log(law);
 console.log(evil);
 let canvas = document.getElementById("result_dot_canvas");
@@ -13,8 +15,6 @@ let context = canvas.getContext("2d");
 background.onload = function(){
     context.drawImage(background, 0, 0, canvas.width, canvas.height);   
 }
-context.lineWidth = 33;
-context.beginPath();
-context.moveTo(law, evil);
-context.lineTo(law, evil + 1);
-context.stroke();
+dot.onload = function(){
+    context.drawImage(dot, 666 - law, evil);   
+}
